cbd0a6ab7ea02deca9805b22d742d6869aa77aec
Nischal Rana <rananischal2011@yahoo.com>
2021-09-04 17:00:34 -0500

    Initial Unchanged App Complete

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"; 
+import Header from "./Header";
+import Create from "./Create";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App () {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return [...prevNotes, newNote];
+        })
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+    <div>
+        <Header />
+        <Create onAdd={addNote} />
+
+        {notes.map((eachNote, index) => {
+            return <Note 
+                key ={index}
+                id = {index}
+                title = {eachNote.title}
+                content = {eachNote.content}
+                onDelete = {deleteNote}
+            />
+        })}
+
+        <Footer />
+    </div>
+    );
+}
+
+export default App;
